4638b0268a32d0030d7bdf0106490658fd8817ce
sabinbabu <sabinacharya444@gmail.com>
2025-02-04 13:11:45 +1100

    duplicate movie not accepted feature added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import SearchResult from "./components/SearchResult";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import FavoriteMovies from "./components/FavoriteMovies";
 
 function App() {
@@ -15,6 +15,17 @@ function App() {
   useEffect(() => {
     localStorage.setItem("movieData", JSON.stringify(movieDataFav));
   }, [movieDataFav]);
+
+  const selectRef = useRef(null);
+  // Add movie to favorite list
+  const handleOnFavBtnClick = () => {
+    // check if movie is already a favorite
+    if (!movieDataFav.some((favMovie) => favMovie.imdbID === movie.imdbID)) {
+      const movieObject = { ...movie, genre: selectRef.current.value };
+      setMovieDataFav((prevMovies) => [...prevMovies, movieObject]);
+      selectRef.current.value = "Select genre";
+    }
+  };
 
   // Remove movie from favorite list
   const handleRemoveBtnClick = (movieID) => {
@@ -35,7 +46,11 @@ function App() {
           className="d-flex flex-column justify-center align-items-center"
         >
           {Object.keys(movie).length ? (
-            <SearchResult movie={movie} setMovieDataFav={setMovieDataFav} />
+            <SearchResult
+              movie={movie}
+              handleOnFavBtnClick={handleOnFavBtnClick}
+              selectRef={selectRef}
+            />
           ) : (
             <Alert variant="warning"> Please Search for a movie</Alert>
           )}
--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,16 +1,10 @@
 /* eslint-disable react/prop-types */
-import { useRef } from "react";
 import { Button, Form, Stack } from "react-bootstrap";
 import MovieCard from "./MovieCard";
 
 const SearchResult = (props) => {
-  const { movie, setMovieDataFav } = props;
-  const selectRef = useRef(null);
-  const handleOnClick = () => {
-    const movieObject = { ...movie, genre: selectRef.current.value };
-    setMovieDataFav((prevMovies) => [...prevMovies, movieObject]);
-    selectRef.current.value = "Select genre";
-  };
+  const { movie, handleOnFavBtnClick, selectRef } = props;
+
   return (
     <>
       <h2>Search Result</h2>
@@ -27,7 +21,7 @@ const SearchResult = (props) => {
           <option value="comedy">Comedy</option>
           <option value="romantic">Romantic</option>
         </Form.Select>
-        <Button onClick={handleOnClick} variant="primary">
+        <Button onClick={handleOnFavBtnClick} variant="primary">
           Add to Fav
         </Button>
       </Stack>
